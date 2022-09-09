1a813967b421a79ad3fce829902f0087f2de89e4
Srini L <sjlaliwala246@gmail.com>
2022-09-09 13:20:41 -0400

    add email sign in and picture to navbar

--- components/navbar/UserNavbar.tsx
+++ components/navbar/UserNavbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { signOut } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 import { NavbarLink } from './NavbarLink'
 
 
@@ -9,13 +9,19 @@ import { NavbarLink } from './NavbarLink'
 interface UserNavbarProps {}
 
 const UserNavbar: React.FC<UserNavbarProps> = ({}: UserNavbarProps) => {
+  const { data: session } = useSession()
 
   return (
     <nav className="sticky top-0 z-10 backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <Link href="/"><a className="text-3xl font-semibold">Hippo</a></Link>
-          <div className="flex space-x-20 text-gray-900">
+          <div className="flex flex-row space-x-20 items-center text-gray-900">
+            <p className='flex flex-row items-center'>
+              Signed in as:
+              {session?.user?.image && <img className='h-10 rounded-full mx-2 shadow-md' src={session.user.image}/>} 
+              {session?.user?.email}
+            </p>
             <NavbarLink title='Profile' href='/my/profile' />
             <NavbarLink title='Log out' href="/api/auth/signout" />
           </div>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -18,18 +18,22 @@ const options = {
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
     }),
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: process.env.EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASSWORD
-    //     }
-    //   },
-    //   from: process.env.EMAIL_FROM
-    // }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
+    }),
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
+  session: {
+    jwt: true
+  },
+  theme: "light"
 };
